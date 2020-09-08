7db62a0e47bb2e60118af37d68968493881aa0ea
Coco <constanze.thoennessen@posteo.de>
2020-09-08 15:22:06 +0200

    My Django Girls app, first commit

---
+++ intro-python.py
@@ -0,0 +1,31 @@
+print ('Hello, Django Girls!')
+print ('Hello, Penny')
+if 3 > 2:
+    print('it works')
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey OLA!')
+elif name == 'Sonja':
+    print ('Hey Sonja!')
+else:
+    print ('Hey anonymous!')
+volume = 57
+if volume < 20:
+    print("Its nice for backgroundmusic")
+elif 20 <= volume < 40:
+    print("Izs blabla")
+elif 40 <= volume < 60:
+    print("Coolio")
+elif 80 <= volume < 100:
+    print("loud")
+else:
+    print ("my ears are hurting")
+# blabla
+def hi()
+    print ('hi there')
+    print('how are you?')
+hi()
